967f97a1c3d804e6d8917c7a539f3084bca0acbc
Jakub <jmrowczynski12@gmail.com>
2019-03-21 23:38:36 +0100

    Selection Sort added

--- SS.py
+++ SS.py
@@ -12,18 +12,16 @@ def convertedList(name):
             t.append(int(i.lstrip()))
     return t
 
-def insertionSort(list):
+def selectionSort(list):
     convList = convertedList(list)
     start = time.time()
-    for j in range(1, len(convList)):
-        key = convList[j]
-
-        i = j - 1
-        while (i >= 0) and (convList[i] > key):
-            convList[i + 1] = convList[i]
-            i -= 1
-        convList[i + 1] = key
+    for j in range(len(convList) - 1, 0, -1):
+        max = j
+        for i in range(j - 1, -1, -1):
+            if convList[i] > convList[max]:
+                max = i
+        convList[j], convList[max] = convList[max], convList[j]
     end = time.time()
     return end - start, convList
 
-print(insertionSort('random 8000.txt'))
+print(selectionSort('random 8000.txt'))
