51c9e6f23124ce0d2e7501822a0dbdb942ca23e6
Jakub <jmrowczynski12@gmail.com>
2019-03-23 19:26:50 +0100

    quick sort added

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from IS import insertionSort as iSort
-from SS import selectionSort as sSort
-from HS import heapSort as hSort
+from ins import insertionSort as iSort
+from ss import selectionSort as sSort
+from hs import heapSort as hSort
+from qs import mainQuickSort as qSort
 from statistics import mean as mean
 
 def main():
     for i in range(1000, 8500, 500):
         print(f"{i} elementów: ", hSort(f'random {i}.txt')[0])
-    # print(iSort('random 8000.txt'))
-    # print(sSort('random 8000.txt'))
-    # print(hSort('random 8000.txt'))
+    print(iSort('random 4000.txt'))
+    print(sSort('random 4000.txt'))
+    print(hSort('random 4000.txt'))
+    print(qSort('random 4000.txt'))
 
 if __name__ == "__main__":
     main()
---
+++ qs.py
@@ -0,0 +1,33 @@
+import time
+from converter import convertedList
+
+def mainQuickSort(list):
+    convList = convertedList(list)
+    start = time.time()
+    quickSort(convList, 0, len(convList) - 1)
+    end = time.time()
+    return end - start, convList
+
+def quickSort(list, p, r):
+    if p < r:
+        q = partition(list, p, r)
+        quickSort(list, p, q)
+        quickSort(list, q + 1, r)
+
+def partition(list, p, r):
+    x = list[(p + r) // 2]
+    i = p - 1
+    j = r + 1
+    while True:
+        while True:
+            j -= 1
+            if list[j] <= x:
+                break
+        while True:
+            i += 1
+            if list[i] >= x:
+                break
+        if i < j:
+            list[i], list[j] = list[j], list[i]
+        else:
+            return j
